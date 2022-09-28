7597f009f2df3cff4388e95e25029a4bab0ca068
Kevin Rodríguez <kevinrodriguezvalentin@gmail.com>
2022-09-28 17:16:22 +0100

    Ionic implanted

--- backend/src/test/java/com/nosequeponerxd/app/InstrumentApplicationTests.java
+++ backend/src/test/java/com/nosequeponerxd/app/InstrumentApplicationTests.java
@@ -1,7 +1,7 @@
 package com.nosequeponerxd.app;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
 
 //@SpringBootTest
 class InstrumentApplicationTests {
